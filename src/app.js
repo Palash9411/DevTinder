c4c26b79b1f9e619e3400366f9e85b5d172eadce
Palash <palashgupta9411@gmail.com>
2026-05-08 08:36:19 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,41 +2,16 @@ const express = require ('express');
 
 const app = express();
 
-
-app.get("/user/:userId/:name",(req,res)=>{
-    const { userId, name } = req.params;
-    console.log(`Fetching user with ID: ${userId} and Name: ${name}`);
-    res.send({firstName: "John", lastName: "Doe", userId, name});
+app.get("/user",(req,res,next)=>{
+    next();
+    res.send("User endpoint");
+},(req,res,next)=>{
+    // res.send("User details");
+    next();
+},(req,res)=>{
+    console.log("This is the third callback");
+    res.send("User details from the third callback");
 });
-
-
-app.get("/user",(req,res)=>{
-    const { userId, name } = req.query;
-    console.log(`Fetching user with ID: ${userId} and Name: ${name}`);
-    res.send({firstName: "John", lastName: "Doe", userId, name});
-});
-
-
-app.get(/^\/user\/a(bc)?d$/, (req, res) => {
-    res.send("Matched!");
-});
-
-app.get("/user/ab*c",(req,res)=>{
-    res.send({firstName: "John", lastName: "Doe"});
-});
-
-app.get("/user/ab*cd",(req,res)=>{
-    res.send({firstName: "John", lastName: "Doe"});
-});
-
-
-app.get(/a/,(req,res)=>{
-    res.send({firstName: "John", lastName: "Doe"});
-});
-
-app.get(/.*fly$/, (req, res) => {
-  res.send('/.*fly$/')
-})
 
 
 // app.use will match all the HTTP method API call to /test 
