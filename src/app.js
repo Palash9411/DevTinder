0197d7d54efe60256f0424d76519da7985f57adb
Palash <palashgupta9411@gmail.com>
2026-05-13 21:25:51 +0530

    init-database-with-post-api-call

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require ('express');
 const connectDB = require('./configs/database');
 const app = express();
+const User = require('./models/user');
+
+app.post('/signup', async (req, res) => {
+    const newUser = new User({
+        firstName : 'palash',
+        lastName: 'gupta',
+        email: 'palash.gupta@example.com',
+        password: 'password123'
+    });
+    try {
+        await newUser.save();
+        res.send('User created successfully');
+    } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 connectDB().then(() => {
     console.log("Database connection established ");
--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,14 @@
 const mongoose= require('mongoose');
-const {Schema} = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 
-const userSchema = Schema({
+const userSchema = new Schema({
     firstName : {
         type: String,
     },
     lastName: {
         type: String,
-    },
+    },  
     email: {
         type: String,
        
