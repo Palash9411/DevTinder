20d8e3d6d689b47d4c49720c248db61762611f1e
Palash <palashgupta9411@gmail.com>
2026-07-05 23:59:26 +0530

    user api

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const connectDB = require('./configs/database');
 const app = express();
 const cookieParser = require('cookie-parser');
-
+const userRoutes = require('./routes/user');
 
 app.use(express.json());
 app.use(cookieParser());
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,19 @@
 const express = require ('express');
 const { userAuth } = require('../middlewares/auth');
 const userRouter = express.Router();
+const connectionRequest = require('../models/connectionRequest');
 
 //get all the connection request for the user 
-userRouter.get('/user/requests',userAuth,async(req,res)=>{
+userRouter.get('/user/requests/recieved',userAuth,async(req,res)=>{
     try{
         const loggedInUser = req.user;
-    }catch{
-
+        const connectionRequests = await connectionRequest.find({
+            toUserId: loggedInUser._id,
+            status: 'interested'
+        }).populate('fromUserId', 'firstName lastName age gender about skills');
+        res.json({ connectionRequests });
+    }catch(error){
+        res.status(500).json({ message: error.message });
     }
 })
 
